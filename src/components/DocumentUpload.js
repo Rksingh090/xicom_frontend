628cad7d0315ad6c3ed5de3fd172355b31fb2ad0
Rishab Singh <rishab.w.singh1234@gmail.com>
2023-07-05 16:10:33 +0530

    sdg

---
+++ src/components/DocumentUpload.js
@@ -0,0 +1,123 @@
+import React, { useRef } from 'react'
+
+
+import { FaPlus } from 'react-icons/fa';
+import { BsUpload } from 'react-icons/bs';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { API } from '../constant';
+
+import axios from 'axios';
+
+const DocumentUpload = ({ file, allFiles, onChangeFile, id, onAddMore, onDelete }) => {
+    const docRef = useRef(null);
+
+    const handleChangeFile = (e) => {
+        const { name, value } = e.target;
+        const fileData = allFiles;
+        fileData[id][name] = value;
+        if (name === "file_type") {
+            fileData[id].file_url = "";
+        }
+        onChangeFile(prev => ({
+            ...prev,
+            files: fileData
+        }))
+    }
+
+    const uploadFile = (e) => {
+        const files = e.target.files;
+        if (!files || files.length <= 0) return;
+        const { type } = files[0];
+
+        const allowedTypes = {
+            pdf: ['application/pdf'],
+            image: ['image/jpeg', 'image/png', 'image/gif']
+        };
+
+        if (file.file_type === "") {
+            alert("Please select file type first.")
+            return;
+        }
+        else if (file.file_type === "pdf") {
+            let isallowed = allowedTypes.pdf.includes(type)
+            if (!isallowed) {
+                alert("Only pdf files is allowed.")
+                return;
+            }
+        }
+        else if (file.file_type === "image") {
+            let isallowed = allowedTypes.image.includes(type)
+            if (!isallowed) {
+                alert("Only Image files is allowed.")
+                return;
+            }
+        }
+
+        const formdata = new FormData();
+        formdata.append("file", files[0]);
+        axios.post(`${API}/upload`, formdata)
+            .then((res) => {
+                const { status, fileUrl } = res.data;
+                console.log(res.data);
+                if (status === "success") {
+                    const fileData = allFiles;
+                    fileData[id].file_url = fileUrl;
+                    onChangeFile(prev => ({
+                        ...prev,
+                        files: fileData
+                    }))
+                }
+            })
+    }
+
+    return (
+        <div className="documentUploadRow">
+            <div className="inputGroup">
+                <label htmlFor="file_name" className='secondary_label'>File Name <span className="requiredMarker">*</span> </label>
+                <input type="text" name="file_name"
+                    value={file.file_name}
+                    onChange={handleChangeFile}
+                    className='formInput' id="file_name" />
+            </div>
+            <div className="inputGroup">
+                <label htmlFor="file_type" className='secondary_label'>Type of File<span className="requiredMarker">*</span> </label>
+                <select name="file_type" className='formInput' id="file_type"
+                    value={file.file_type}
+                    onChange={handleChangeFile}
+                >
+                    <option value="" disabled>Choose File Type</option>
+                    <option value="image">Image</option>
+                    <option value="pdf">Pdf</option>
+                </select>
+                <span className="infoText">(image, pdf.)</span>
+            </div>
+            <div className="inputGroup">
+                <label htmlFor="file_url" className='secondary_label'>Uplolad Document <span className="requiredMarker">*</span> </label>
+                <div className='fileInputDiv'
+                    onClick={() => docRef.current && docRef.current?.click()}
+                >
+                    <p>{file?.file_url || ""}</p>
+                    <div>
+                        <BsUpload size={20} />
+                    </div>
+                    <input ref={docRef} onChange={uploadFile} type="file" hidden name="file_url" className='formInput' id="file_url" />
+                </div>
+            </div>
+            {
+                id === 0 ? (
+                    <div className="addMoreFilesBtn" onClick={onAddMore}>
+                        <FaPlus size={20} />
+                    </div>
+
+                ) : (
+
+                    <div className="deleteThisDoc" onClick={onDelete}>
+                        <AiOutlineDelete size={20} />
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default DocumentUpload
---
+++ src/components/XicomForm.js
@@ -0,0 +1,266 @@
+import React, { useState } from 'react'
+
+import { API } from '../constant';
+import axios from 'axios';
+
+import DocumentUpload from './DocumentUpload';
+
+const XicomForm = () => {
+
+
+    const [formData, setFormData] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        dob: "",
+        residential_address: {
+            street_1: "",
+            street_2: ""
+        },
+        same_addresses: false,
+        permanent_address: {
+            street_1: "",
+            street_2: ""
+        },
+        files: [
+            {
+                file_name: "",
+                file_type: "",
+                file_url: ""
+            }
+        ]
+    })
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+
+        const currentData = new Date();
+        const dobDate = new Date(formData.dob);
+
+        const diffInMilliseconds = currentData - dobDate;
+        const millisecondsPerYear = 1000 * 60 * 60 * 24 * 365.25;
+        const diffInYears = diffInMilliseconds / millisecondsPerYear;
+
+
+        if (diffInYears < 18) {
+            alert("You must be 18 years old.")
+            return;
+        }
+
+        // api call to add the form 
+        axios.post(`${API}/register`, formData)
+            .then((res) => {
+                const { status } = res.data;
+                if (status === "success") {
+                    alert("Data adedd!")
+                }
+            })
+            .catch((err) => {
+                alert(err.response.data?.message)
+            })
+    }
+
+
+    // add new files 
+    const addMoreFiles = () => {
+        setFormData(prev => ({
+            ...prev,
+            files: [
+                ...prev.files,
+                {
+                    file_name: "",
+                    file_type: "",
+                    file_url: ""
+                }
+            ]
+        }))
+    }
+
+    const deleteDocument = (idx) => {
+        const allFiles = formData.files.filter((_, doc_idx) => doc_idx !== idx);
+        setFormData(prev => ({
+            ...prev,
+            files: allFiles
+        }))
+    }
+
+
+    const handleFormInput = (e) => {
+        const { name, value } = e.target;
+        setFormData(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    }
+
+    return (
+        <div className="xicomFormPage">
+            <form onSubmit={onFormSubmit} className='mainForm'>
+                <div>
+                    <h2>Xicom Technologies</h2>
+                </div>
+                <div className="gridCol2">
+                    <div className="inputGroup">
+                        <label className="labelStyling" htmlFor="first_name">First Name<span className='requiredMarker'>*</span></label>
+                        <input
+                            value={formData.first_name}
+                            onChange={handleFormInput}
+                            type="text" name='first_name' id='first_name' className="formInput" placeholder='Enter your first name here...'
+                            required
+                        />
+                    </div>
+                    <div className="inputGroup">
+                        <label className="labelStyling" htmlFor="last_name">Last Name<span className='requiredMarker'>*</span></label>
+                        <input
+                            value={formData.last_name}
+                            onChange={handleFormInput}
+                            type="text" className="formInput" name='last_name' id='last_name' placeholder='Enter your last name here...'
+                            required
+                        />
+                    </div>
+                </div>
+                <div className="gridCol2">
+                    <div className="inputGroup">
+                        <label className="labelStyling" htmlFor="email">Email<span className='requiredMarker'>*</span></label>
+                        <input
+                            value={formData.email}
+                            onChange={handleFormInput}
+                            type="email"
+                            name='email' id='email' className="formInput" placeholder='ex:myname2example.com'
+                            required
+                        />
+                    </div>
+                    <div className="inputGroup">
+                        <label className="labelStyling" htmlFor="dob">Date of Birth<span className='requiredMarker'>*</span></label>
+                        <input
+                            value={formData.dob}
+                            onChange={handleFormInput}
+                            type="date" className="formInput"
+                            onFocus={(e) => (e.target.type = "date")}
+                            onBlur={(e) => (e.target.type = "text")}
+                            name='dob' id='dob' placeholder='Date of Birth' />
+                        <span className="infoText">(Min. age should be greater than 18years.)</span>
+                    </div>
+                </div>
+
+                {/* residential address  */}
+                <div className='residentialAddress'>
+                    <h2 className='labelStyling'>Residential Address</h2>
+                    <div className="gridCol2">
+                        <div className="inputGroup">
+                            <label className="secondary_label" htmlFor="street_1">Street 1<span className='requiredMarker'>*</span></label>
+                            <input type="text"
+                                value={formData.residential_address.street_1}
+                                onChange={(e) => setFormData(prev => ({
+                                    ...prev,
+                                    residential_address: {
+                                        ...prev.residential_address,
+                                        street_1: e.target.value
+                                    }
+                                }))}
+                                name='street_1' id='street_1' className="formInput"
+                                required
+                            />
+                        </div>
+                        <div className="inputGroup">
+                            <label className="secondary_label" htmlFor="street_2">Street 2<span className='requiredMarker'>*</span></label>
+                            <input type="text"
+                                value={formData.residential_address.street_2}
+                                onChange={(e) => setFormData(prev => ({
+                                    ...prev,
+                                    residential_address: {
+                                        ...prev.residential_address,
+                                        street_2: e.target.value
+                                    }
+                                }))}
+                                name='street_2' id='street_2' className="formInput"
+                                required
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className='sameAddressCheckBox'>
+                    <input
+                        value={formData.same_addresses}
+                        onChange={(e) => setFormData(prev => ({
+                            ...prev,
+                            same_addresses: !prev.same_addresses
+                        }))}
+                        type="checkbox" name="same_addresses" id="same_addresses" />
+                    <label htmlFor="same_addresses">Same as Residential Address</label>
+                </div>
+
+                {/* permanent address  */}
+                <div className='residentialAddress'>
+                    <h2 className='labelStyling'>Permanent Address</h2>
+                    <div className="gridCol2">
+                        <div className="inputGroup">
+                            <label className="secondary_label" htmlFor="p_street_1">Street 1</label>
+                            <input
+                                value={formData.permanent_address.street_1}
+                                onChange={(e) => setFormData(prev => ({
+                                    ...prev,
+                                    permanent_address: {
+                                        ...prev.permanent_address,
+                                        street_1: e.target.value
+                                    }
+                                }))}
+                                type="text" name='street_1' id='p_street_1' className="formInput"
+
+                                required={!formData.same_addresses}
+                            />
+                        </div>
+                        <div className="inputGroup">
+                            <label className="secondary_label" htmlFor="p_street_2">Street 2</label>
+                            <input type="text"
+                                value={formData.permanent_address.street_2}
+                                onChange={(e) => setFormData(prev => ({
+                                    ...prev,
+                                    permanent_address: {
+                                        ...prev.permanent_address,
+                                        street_2: e.target.value
+                                    }
+                                }))}
+                                name='street_2' id='p_street_2' className="formInput"
+                                required={!formData.same_addresses}
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                {/* DOCUMENT UPLOAD AREA  */}
+                <div className="documentsUpload">
+                    <h2 className="labelStyling">Documents Upload</h2>
+
+                    {
+                        formData.files &&
+                        formData.files.length > 0 &&
+                        formData.files.map((file, idx) => {
+                            return (
+                                <DocumentUpload
+                                    onChangeFile={setFormData}
+                                    key={idx}
+                                    file={file}
+                                    id={idx}
+                                    onAddMore={addMoreFiles}
+                                    onDelete={() => deleteDocument(idx)}
+                                    allFiles={formData.files}
+                                />
+
+                            )
+                        })
+                    }
+                </div>
+
+                <div className='formSubmitBtn'>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+
+
+export default XicomForm
