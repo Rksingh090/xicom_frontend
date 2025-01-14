ec8c6f490f2118602ec3e171b049fe7c2308a6c9
Rishab Singh <54115599+Rksingh090@users.noreply.github.com>
2025-01-14 20:08:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import {useEffect} from 'react';
 import XicomForm from './components/XicomForm';
+import {API} from './constants';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+    await axios.get(`${API}/register`);
+    }
+    getData();
+  }, []);
   return (
     <div className="App">
       <XicomForm />
